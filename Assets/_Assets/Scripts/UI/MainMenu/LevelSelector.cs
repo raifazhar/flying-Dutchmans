95e92257e3ae56e839920bd0b42fd5cca61ff229
Kooale325 <muhammad.bilal832017@gmail.com>
2024-08-04 15:45:56 +0500

    main menu performance fixed

--- Assets/_Assets/Scripts/UI/MainMenu/LevelSelector.cs
+++ Assets/_Assets/Scripts/UI/MainMenu/LevelSelector.cs
@@ -1,35 +1,28 @@
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class LevelSelector : MonoBehaviour {
-    public static LevelSelector Instance {
-        get; private set;
-    }
+    public static LevelSelector Instance { get; private set; }
+
     [SerializeField] private float offsetLerpSpeed = 1f;
-    private float effectiveVerticalOffset = 0f;
-    private float inputVerticalOffset = 0f;
     [SerializeField] private float verticalMultiplier = 1f;
     [SerializeField] private LevelListSO levelsSO;
     [SerializeField] private Transform levelIconPrefab;
     [SerializeField] private int poolSize = 20;
     [SerializeField] private float verticalPadding = 2f;
-    private Vector2 verticalBounds;
-    private Vector2 softBounds;
-    private float[] activeIconCoords;
-
-
-    //Set bounds
-    private float upperBound;
-    private float lowerBound;
-    private Transform[] activeIcons;
     [SerializeField] private Mesh[] visualMeshes;
     [SerializeField] private Material meshMaterial;
+
+    private float effectiveVerticalOffset = 0f;
+    private float inputVerticalOffset = 0f;
+    private int targetFrameRate = 60;
+    private Vector2 verticalBounds;
+    private Transform[] activeIcons;
     private Vector2 prevPos;
     private Vector2 currPos;
+
+    private float upperBound;
+    private float lowerBound;
 
     private void Awake() {
         if (Instance == null) {
@@ -38,21 +31,28 @@ public class LevelSelector : MonoBehaviour {
         else {
             Destroy(gameObject);
         }
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = targetFrameRate;
+        Screen.orientation = ScreenOrientation.Portrait;
     }
+
     private void Start() {
         activeIcons = new Transform[poolSize];
         for (int i = 0; i < poolSize; i++) {
             Transform icon = Instantiate(levelIconPrefab, transform);
             icon.gameObject.SetActive(false);
             activeIcons[i] = icon;
+            icon.GetComponent<LevelIcon>().SetMaterial(meshMaterial);
         }
         prevPos = Vector2.zero;
         currPos = Vector2.zero;
         verticalBounds.y = 0f;
         verticalBounds.x = -(levelsSO.levels.Count * verticalPadding) + (verticalPadding * 2f);
         SetCameraFrustumBounds();
+        HandleOffset();
+        HandleIcons();
+        gameObject.SetActive(false);
     }
-
 
     private void Update() {
         HandleOffset();
@@ -73,70 +73,48 @@ public class LevelSelector : MonoBehaviour {
         }
         inputVerticalOffset = Mathf.Clamp(inputVerticalOffset, verticalBounds.x, verticalBounds.y);
         effectiveVerticalOffset = Mathf.Lerp(effectiveVerticalOffset, inputVerticalOffset, Time.deltaTime * offsetLerpSpeed);
-
     }
 
-
     private void SetCameraFrustumBounds() {
-        //Do 2 raycasts, one at the top and one at the bottom of the screen, and get the z value of the hit point   
         Plane worldPlane = new Plane(Vector3.up, Vector3.zero);
         Ray topRay = Camera.main.ScreenPointToRay(new Vector3(0, Screen.height, 0));
         Ray bottomRay = Camera.main.ScreenPointToRay(new Vector3(0, 0, 0));
         float topHit = 0f;
         float bottomHit = 0f;
+
         if (worldPlane.Raycast(topRay, out topHit)) {
             upperBound = topRay.GetPoint(topHit).z;
         }
         if (worldPlane.Raycast(bottomRay, out bottomHit)) {
             lowerBound = bottomRay.GetPoint(bottomHit).z;
         }
-
     }
 
-    //This method is used for setting the state, things like the levelindex, x position, model etc of the activeIcon
-    private void SetIconState(int arrIndex, int levelIndex) {
-        activeIcons[arrIndex].gameObject.SetActive(true);
-        Random.InitState(levelIndex);
-        float x = Random.Range(-1.7f, 1.7f);
-        float rotation = Random.Range(0f, 360f);
-        activeIcons[arrIndex].position = new Vector3(x, 0, 0);
-        activeIcons[arrIndex].GetComponent<LevelIcon>().SetVisualMesh(visualMeshes[Random.Range(0, visualMeshes.Length)]);
-        activeIcons[arrIndex].GetComponent<LevelIcon>().SetVisualRotation(Quaternion.Euler(0, rotation, 0));
-        activeIcons[arrIndex].GetComponent<LevelIcon>().SetMaterial(meshMaterial);
-        activeIcons[arrIndex].GetComponent<LevelIcon>().SetLevelIndex(levelIndex);
-    }
     private void HandleIcons() {
-        //Step 1, check which icons should be inside the view frustum based on vertical offset
         float upperBoundWorldSpace = upperBound - effectiveVerticalOffset;
         float lowerBoundWorldSpace = lowerBound - effectiveVerticalOffset;
-        //we can divide them by the vertical padding to get the index of the first and last icon that should be visible
+
         int firstVisibleIndex = Mathf.FloorToInt(lowerBoundWorldSpace / verticalPadding);
         int lastVisibleIndex = Mathf.FloorToInt(upperBoundWorldSpace / verticalPadding);
 
-        if (firstVisibleIndex < 0) {
-            firstVisibleIndex = 0;
-        }
-        if (lastVisibleIndex > levelsSO.levels.Count - 1) {
-            lastVisibleIndex = levelsSO.levels.Count - 1;
-        }
-        //Check if any active icons fall out of this range and disable them
+        firstVisibleIndex = Mathf.Clamp(firstVisibleIndex, 0, levelsSO.levels.Count - 1);
+        lastVisibleIndex = Mathf.Clamp(lastVisibleIndex, 0, levelsSO.levels.Count - 1);
+
+        // Deactivate out-of-bound icons
         for (int i = 0; i < poolSize; i++) {
-            if (!activeIcons[i].gameObject.activeSelf) {
-                continue;
-            }
-            int index = activeIcons[i].GetComponent<LevelIcon>().GetLevelIndex();
-            if (index < firstVisibleIndex || index > lastVisibleIndex) {
-                activeIcons[i].gameObject.SetActive(false);
+            if (activeIcons[i].gameObject.activeSelf) {
+                int index = activeIcons[i].GetComponent<LevelIcon>().GetLevelIndex();
+                if (index < firstVisibleIndex || index > lastVisibleIndex) {
+                    activeIcons[i].gameObject.SetActive(false);
+                }
             }
         }
-        //Make a list of all indexes that should fall within range and if they are not active, 
-        //make an inactive icon become active and set its index
+
+        // Activate or update in-bound icons
         for (int i = firstVisibleIndex; i <= lastVisibleIndex; i++) {
             bool found = false;
             for (int j = 0; j < poolSize; j++) {
-                int index = activeIcons[j].GetComponent<LevelIcon>().GetLevelIndex();
-                if (index == i) {
-                    activeIcons[j].gameObject.SetActive(true);
+                if (activeIcons[j].gameObject.activeSelf && activeIcons[j].GetComponent<LevelIcon>().GetLevelIndex() == i) {
                     found = true;
                     break;
                 }
@@ -151,16 +129,25 @@ public class LevelSelector : MonoBehaviour {
             }
         }
 
-        //Step 2, update the position of all active icons as per their index
+        // Update positions of active icons
         for (int i = 0; i < poolSize; i++) {
             if (activeIcons[i].gameObject.activeSelf) {
                 int index = activeIcons[i].GetComponent<LevelIcon>().GetLevelIndex();
                 activeIcons[i].position = new Vector3(activeIcons[i].position.x, activeIcons[i].position.y, index * verticalPadding + effectiveVerticalOffset);
             }
         }
+    }
 
-        SoundManager.Playsound(SoundManager.Sound.Victory);
-
+    private void SetIconState(int arrIndex, int levelIndex) {
+        activeIcons[arrIndex].gameObject.SetActive(true);
+        Random.InitState(levelIndex);
+        float x = Random.Range(-1.7f, 1.7f);
+        float rotation = Random.Range(0f, 360f);
+        LevelIcon icon = activeIcons[arrIndex].GetComponent<LevelIcon>();
+        icon.SetVisualMesh(visualMeshes[Random.Range(0, visualMeshes.Length)]);
+        icon.SetVisualRotation(Quaternion.Euler(0, rotation, 0));
+        icon.SetLevelIndex(levelIndex);
+        activeIcons[arrIndex].position = new Vector3(x, 0, 0);
     }
 
     public float GetVerticalOffset() {
